662af790a2fe04d7c64c21a62e3f3cadfdd29dda
Greg Swallow <gswallow@gmail.com>
2016-06-13 11:50:02 -0400

    [AR-1869] Fix HTTP/415 errors being returned by New Relic.

--- ansible/roles/newrelic/templates/usr/sbin/newrelic_lv_thinpool.sh
+++ ansible/roles/newrelic/templates/usr/sbin/newrelic_lv_thinpool.sh
@@ -12,25 +12,25 @@ LVS="/sbin/lvs --noheadings -o lv_name,data_percent,metadata_percent"
 while read -r line; do
   read -a arr <<< $line
   if [[ ! -z "${arr[1]}" && ! -z "${arr[2]}" ]]; then
-    curl -s https://platform-api.newrelic.com/platform/v1/metrics \
+    curl -s "https://platform-api.newrelic.com/platform/v1/metrics" \
     -H "X-License-Key: ${LICENSE_KEY}" \
     -H "Content-Type: application/json" \
     -H "Accept: application/json" \
     -o /dev/null \
     -X POST -d '{
       "agent": {
-        "host" : "'"$HOST"'",
-        "pid" : '"$$"',
+        "host" : "'$HOST'",
+        "pid" : '$$',
         "version" : "1.0.0"
       },
       "components": [
         {
-          "name": "'"$HOST"'",
-          "guid": "'"$GUID"'",,
+          "name": "'$HOST'",
+          "guid": "'$GUID'",
           "duration" : 60,
           "metrics" : {
-            "Component/Thin-Pool/'"${arr[0]}"'/Data/Used[percent]": '"${arr[1]}"',
-            "Component/Thin-Pool/'"${arr[0]}"'/Metadata/Used[percent]": '"${arr[2]}"'
+            "Component/Thin-Pool/'${arr[0]}'/Data/Used[percent]": '${arr[1]}',
+            "Component/Thin-Pool/'${arr[0]}'/Metadata/Used[percent]": '${arr[2]}'
           }
         }
       ]
